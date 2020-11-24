e9f56b301267765dfdb3e15d5e9a15fe9acf5383
= <=>
2020-11-23 21:01:56 -0300

    initial commit

---
+++ src/main/java/com/valdir/apilibrary/ApiLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.apilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valdir/apilibrary/ApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdir.apilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
