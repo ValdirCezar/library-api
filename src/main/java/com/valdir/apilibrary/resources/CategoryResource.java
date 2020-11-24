8a1d67c01612cb88cfb98f83e1b07ac67adf77c5
= <=>
2020-11-24 08:06:06 -0300

    swagger configured and tested

--- src/main/java/com/valdir/apilibrary/resources/CategoryResource.java
+++ src/main/java/com/valdir/apilibrary/resources/CategoryResource.java
@@ -1,10 +1,24 @@
 package com.valdir.apilibrary.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/api/categories")
 public class CategoryResource {
+	
+	@ApiOperation(value = "return a example of REST OK")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Succes method return")
+	})
+	@GetMapping
+	public String find() {
+		return "REST OK";
+	}
 
 }
