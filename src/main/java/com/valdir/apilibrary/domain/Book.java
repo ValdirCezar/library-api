866e4487ecc8f44afc6420b95d70384cb5e1d67e
= <=>
2020-11-23 21:21:34 -0300

    H2 configured and relationship between category - book

--- src/main/java/com/valdir/apilibrary/domain/Book.java
+++ src/main/java/com/valdir/apilibrary/domain/Book.java
@@ -4,13 +4,28 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Book implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 
+	@ManyToMany
+	@JoinTable(name = "BOOK_CATEGORY",
+			joinColumns = @JoinColumn(name = "book_id"),
+			inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private List<Category> categories = new ArrayList<>();
 
 	public Book() {
--- src/main/java/com/valdir/apilibrary/domain/Category.java
+++ src/main/java/com/valdir/apilibrary/domain/Category.java
@@ -4,13 +4,23 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 
+	@ManyToMany(mappedBy = "categories")
 	private List<Book> books = new ArrayList<>();
 
 	public Category() {
