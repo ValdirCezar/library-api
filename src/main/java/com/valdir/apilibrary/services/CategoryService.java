972426761b3d2ab8c0a8a0f6255d72855b6b3505
= <=>
2020-11-23 21:31:22 -0300

    service to Category and Book created

---
+++ src/main/java/com/valdir/apilibrary/services/CategoryService.java
@@ -0,0 +1,10 @@
+package com.valdir.apilibrary.services;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/categories")
+public class CategoryService {
+
+}
