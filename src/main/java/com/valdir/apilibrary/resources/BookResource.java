6ce9cbfe5911bf58f8468809f9a8fab9811f007c
= <=>
2020-11-25 16:28:50 -0300

    adjustemnts

--- src/main/java/com/valdir/apilibrary/resources/BookResource.java
+++ src/main/java/com/valdir/apilibrary/resources/BookResource.java
@@ -1,9 +1,31 @@
 package com.valdir.apilibrary.resources;
 
+import java.net.URI;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.valdir.apilibrary.domain.Book;
+import com.valdir.apilibrary.services.BookService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value = "/api")
 @CrossOrigin("*")
-public class {
+public class BookResource {
 	
 	@Autowired
 	private BookService service;
